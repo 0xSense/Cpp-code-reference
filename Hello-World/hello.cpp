601815d671589f6cc3e4af6f8048f309aa80afff
Sense <105939929+0xSense@users.noreply.github.com>
2023-10-19 13:44:37 -0400

    build: hello world

---
+++ Hello-World/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// A C++ Program to output a greeting
+
+// main is a mandatory function for cpp programs
+// programs can contain many functions but must always have a main
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+};
