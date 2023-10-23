09e3036decde0a11816ec2b2536b9ad350a76b07
0xSense <safewithcrypto@gmail.com>
2023-10-22 20:45:32 -0400

    main functions

---
+++ Knowledge-challenge/knowledge.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // data types
+    float num1 = 8.99;
+    string text = "hello from c++\n";
+    bool isTrue = true;
+
+    cout << text;
+    cout << isTrue;
+    return 0;
+}
+
+int print_statements(){
+    return 0;
+}
